111b0f19faa5a9248c528b2cf3776877f8fd1bd6
Natalia Kabała <nkabala@iteratorshq.com>
2024-02-12 16:35:03 +0100

    feature: add emotion

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,20 @@
 import type { ReactNode } from "react";
+import * as Styled from "./Title.styled";
+import { ThemeProvider, useTheme } from "@emotion/react";
+import theme from "./theme";
 
 interface TitleProps {
   title: string;
 }
 
 function Title({ title }: TitleProps): JSX.Element {
-  return <h1>{title}</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Styled.Title>
+        <Styled.Text>{title}</Styled.Text>
+      </Styled.Title>
+    </ThemeProvider>
+  );
 }
 
 export default Title;
