84e72983ae45337caa680f4d6fe7f5a75e11c50a
Natalia Kabała <nkabala@iteratorshq.com>
2024-03-29 15:11:39 +0100

    feature: add Conferences section

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,17 +1,21 @@
 import Books from '@images/books.png';
 import HeroBg from '@images/hero_bg.png';
+import LambdaDays from '@images/lambda-days.png';
 import MeetupComputer from '@images/meetup-computer.png';
 import MeetupGroup from '@images/meetup-group.png';
 import NetguruLogo from '@images/netguru-logo.png';
+import ScalaConference from '@images/scala-conference.png';
 import Scala from '@images/scala.png';
 import Slack from '@images/slack.png';
 
 export const IMAGES = {
   books: Books,
   heroBg: HeroBg,
+  lambdaDays: LambdaDays,
   meetupComputer: MeetupComputer,
   meetupGroup: MeetupGroup,
   netguruLogo: NetguruLogo,
+  scalaConference: ScalaConference,
   scala: Scala,
   slack: Slack,
 };
